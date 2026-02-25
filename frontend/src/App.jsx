efec6107895c5657b978d6479787dc690b01335a
Petar Yankov <yankov.petar4@gmail.com>
2026-02-25 16:03:55 +0400

    add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router';
+import Dashboard from './components/dashboard/Dashboard';
+import Login from './components/users/login/Login';
+import Register from './components/register/Register';
+import Home from './components/home/Home';
+import './App.css';
 
 function App() {
 
+
   return (
     <>
-      <h1>Digital Banking Platform</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </>
   )
 }
 
-export default App
+export default App;
